8c7ae774fb0c128e2483bed7405a1dd4a96e67c9
Matei2807 <92317040+Matei2807@users.noreply.github.com>
2024-05-14 23:29:12 +0300

    Update friends.h

--- friends.h
+++ friends.h
@@ -9,6 +9,22 @@
  *
  * Please add any necessary parameters to the functions
 */
-void handle_input_friends(char *input);
+void add_friend(int ***relations, char *user1, char *user2);
+
+void remove_friend(int ***relations, char *user1, char *user2);
+
+void sort_names(char ***names, int names_number);
+
+void print_suggestions(int **relations, char *user);
+
+void print_distance(int **relations, char *user1, char *user2);
+
+void print_common_friends(int **relations, char *user1, char *user2);
+
+void print_friends(int **relations, char *user);
+
+void print_popular(int **relations, char *user);
+
+void handle_input_friends(char *input, int ***relations);
 
 #endif // FRIENDS_H
