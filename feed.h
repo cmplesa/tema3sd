53b83807e222c11e4ee00815de11250a87cbd890
cmplesa <151476582+cmplesa@users.noreply.github.com>
2024-05-16 22:42:57 +0300

    Update feed.h

--- feed.h
+++ feed.h
@@ -1,11 +1,16 @@
 #ifndef FEED_H
 #define FEED_H
 
+#include "posts.h"
+#include "friends.h"
+
 /**
  * Function that handles the calling of every command from task 3
  *
  * Please add any necessary parameters to the functions
 */
-void handle_input_feed(char *input);
+
+
+void handle_input_feed(char *input, post_array_t **posts, int ***relations);
 
 #endif // FEED_H
