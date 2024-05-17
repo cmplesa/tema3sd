ed9ee1562b2936e65d0b2f6d214a01e23b1bedc9
Matei2807 <92317040+Matei2807@users.noreply.github.com>
2024-05-17 19:13:22 +0300

    Update feed.h

--- feed.h
+++ feed.h
@@ -4,11 +4,6 @@
 #include "posts.h"
 #include "friends.h"
 
-/**
- * Function that handles the calling of every command from task 3
- *
- * Please add any necessary parameters to the functions
-*/
 void handle_input_feed(char *input, post_array_t **posts, int ***relations);
 
 #endif // FEED_H
